5dd363764360a0ce69ea6ab9e407c0014084b978
Irfan <irfansk.se@gmail.com>
2021-04-19 11:13:07 +0530

    basic crud operations for tours complete

--- app.js
+++ app.js
@@ -100,6 +100,45 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+/**
+ * We have to verbs to update the data PUT and PATCH 
+ * with PUT we accept that our application receives the entire 
+ * new updated object,
+ * 
+ * PATCH only updates the data that is changing.
+ * 
+ */
+app.patch('/api/v1/tours/:id', (req, res) => {
+
+  if ((req.params.id * 1) > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>'
+    }
+  });
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if ((req.params.id * 1) > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`);
