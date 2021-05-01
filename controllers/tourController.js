d834d38b1d93072b8000f2203a564c5952bf2197
Irfan <irfansk.se@gmail.com>
2021-05-01 12:24:39 +0530

    api features filtering,sorting,limiting,pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,8 +2,65 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTour = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    //BUILD QUERY
+    // 1A) FILTERING
 
+    //hard copying query object into the new variable
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    // 1B) ADVANCED FILTERING
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    // console.log(JSON.parse(queryStr));
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) SORTING
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+      //sort('price ratingsAverage');
+    }
+    // else {
+    //   query = query.sort('-createdAt');  //this piece of code is causing an unexpected bug in pagination
+    // }
+
+    // ANOTHER WAY OF SORTING
+    // query = req.query.sort
+    //   ? query.sort(req.query.sort.replace(/,/g, ' '))
+    //   : query.sort('-createdAt');
+
+    // 3)FIELD LIMITING
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+      // query.select('name duration price');
+    } else {
+      //minus sign will exclude __v field
+      query = query.select('-__v');
+    }
+
+    // 4)PAGINATION
+
+    const page = req.query.page * 1 || 1; //default value = 1
+    const limit = req.query.limit * 1 || 100; //default value = 100
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) {
+        throw new Error('This page does not exit!');
+      }
+    }
+
+    //EXECUTE QUERY
+    const tours = await query;
+
+    //SEND RESPONSE
     res.status(200).json({
       status: 'success',
       requestedAt: req.requestTime,
